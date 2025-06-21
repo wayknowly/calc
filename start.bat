ae4e3575443053a78de2b3c3841df0554feb3657
typicalknowly <86556143+wayknowly@users.noreply.github.com>
2025-06-21 12:31:58 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
 @echo off
-start cmd /k python "C:\programms\calc\main.py" # Если не запускается код, нужно поменять директорию кода внутри кавычек
+start cmd /k python "C:\calc\main.py" # Если не запускается код, нужно поменять директорию кода внутри кавычек
